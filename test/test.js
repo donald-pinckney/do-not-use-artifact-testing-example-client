0eb052be9ee1b43b83d70d7ecd82ed46ac889ad7
Donald Pinckney <donald_pinckney@icloud.com>
2023-09-28 13:54:20 +0200

    Testing client for the testing lib. This client fails tests with 1.0.1 of the lib!

---
+++ test/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const addStrings = require('..').addStrings;
+
+describe('addStrings', function () {
+    it('should return 5 when given \'2\' and \'3\'', function () {
+        assert.equal(addStrings('2', '3'), 5);
+    });
+
+    it('should return 5 when given \'0x02\' and \'0x03\'', function () {
+        assert.equal(addStrings('0x02', '0x03'), 5);
+    });
+});
